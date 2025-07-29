30a467374e2c9fa1825dbb917c6fcc91b6ba2bc0
Tia Piol <tiarox02@yahoo.com>
2025-07-29 09:01:38 -0700

    build destinations landing page

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -3,4 +3,17 @@ const router = express.Router();
 
 const Destination = require("../models/destination");
 
+//                          ROUTES
+
+// GET /destinations
+router.get("/", async (req, res) => {
+try {
+    const allDestinations = await Destination.find({}).populate("country");
+    res.render("destinations/index.ejs")
+} catch (error) {
+    console.log(error);
+    res.redirect("/")
+}
+});
+
 module.exports = router;
