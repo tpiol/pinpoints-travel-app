8a401e0225216050631659dd1f74a2e8911c7e27
Tia Piol <tiarox02@yahoo.com>
2025-07-30 12:01:06 -0700

    built show page for view destinations

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -7,24 +7,24 @@ const Destination = require("../models/destination");
 
 // GET /destinations
 router.get("/", async (req, res) => {
-try {
-    const allDestinations = await Destination.find({}).populate("country");
-    res.render("destinations/index.ejs", {
-        allDestinations: allDestinations,
-    })
-} catch (error) {
-    console.log(error);
-    res.redirect("/")
-}
+    try {
+        const allDestinations = await Destination.find({}).populate("country");
+        res.render("destinations/index.ejs", {
+            allDestinations: allDestinations,
+        })
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
+    }
 });
 
 // GET /destinations/:destinationId
 router.get("/", async (req, res) => {
     try {
-        const currentDestination = await Destination.findById(req.session.destination._id);
-        const destination = currentDestination.notes.id(req.params.DestinationId);
+        const currentDestination = await Destination.findById(req.params.destinationId).populate("country");
+        console.log(currentDestination);
         res.render("destinations/show.ejs", {
-            notes: destination,
+            destination: currentDestination,
         });
     } catch (error) {
         console.log(error);
@@ -32,10 +32,39 @@ router.get("/", async (req, res) => {
     }
 });
 
-//GET /destinations/:destinationId/notes
 
+// GET /destinations/new
+router.get("/new", (req, res) => {
+    try {
+    
+        res.render("destinations/new.ejs")
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
 
-// GET /destinations/:destinationId/notes/new
+// POST /destinations/
+router.post("/", async (req, res) => {
+try {
+req.body.owner = req.session.user._id;
+await Destination.create(req.body);
+res.redirect("/destinations")
+} catch (error) {
+    console.log(error);
+    res.redirect("/")
+}
+})
 
-
+// GET /destinations/:destinationId/notes
+router.get("/:destinationId/notes", async (req, res) => {
+    try {
+ const currentDestination = await Destination.findById(req.params.destinationId)
+ const destination = currentDestination.destinations_id(req.params.destinationId);
+ res.render("destinations/show.ejs")
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
+    }
+})
 module.exports = router;
