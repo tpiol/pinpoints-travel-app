31696f76f33728a64e4212d38016a1fc11fc8268
Tia Piol <tiarox02@yahoo.com>
2025-07-30 07:49:53 -0700

    destination show page

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -9,11 +9,33 @@ const Destination = require("../models/destination");
 router.get("/", async (req, res) => {
 try {
     const allDestinations = await Destination.find({}).populate("country");
-    res.render("destinations/index.ejs")
+    res.render("destinations/index.ejs", {
+        allDestinations: allDestinations,
+    })
 } catch (error) {
     console.log(error);
     res.redirect("/")
 }
 });
+
+// GET /destinations/:destinationId
+router.get("/", async (req, res) => {
+    try {
+        const currentDestination = await Destination.findById(req.session.destination._id);
+        const destination = currentDestination.notes.id(req.params.DestinationId);
+        res.render("destinations/show.ejs", {
+            notes: destination,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+//GET /destinations/:destinationId/notes
+
+
+// GET /destinations/:destinationId/notes/new
+
 
 module.exports = router;
