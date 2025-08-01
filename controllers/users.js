80fb772d9edd8d5ab93d77e6704c06ade13f925c
Tia Piol <tiarox02@yahoo.com>
2025-07-31 19:35:35 -0700

    added a user profile page

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,31 @@ const Destination = require("../models/destination");
 //              Routes
 
 // GET 
-router.get("/profile", (req, res) => {
+router.get("/profile", async (req, res) => {
     try {
-        res.send("Profile Page")
+        const myNotes = await Destination.find({ "notes.authorId": req.session.user._id }).populate("notes.authorId")
+        const myFavoriteNotes = await Destination.find({ "notes.favoritedBy": req.session.user._id }).populate("notes.authorId")
+        let noteCount = 0;
+        let allMyNotes = [];
+        myNotes.forEach(destination => {
+            destination.notes.forEach(note => {
+                if (note.authorId.toString() === req.session.user._id.toString()) {
+                    noteCount++
+                    allMyNotes.push(note)
+                }
+            });
+        });
+
+        res.render("users/show.ejs", {
+            myNotes,
+            myFavoriteNotes,
+            noteCount,
+            allMyNotes,
+        });
     } catch (error) {
         console.log(error);
         res.redirect("/");
-    }
-})
+    };
+});
 
 module.exports = router;
